aca2640e3ed67d9a3474c136f25f24303ae9bfc7
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2021-12-09 01:30:40 +0530

    Initial setup

---
+++ src/test/java/com/aimit/campushire/CampushireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimit.campushire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
