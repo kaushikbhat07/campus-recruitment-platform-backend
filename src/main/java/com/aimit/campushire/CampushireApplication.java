d7e492919bc38d642ec4901f1212604940d6423d
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-01-11 00:10:04 +0530

    Heroku deploy test

--- src/main/java/com/aimit/campushire/CampushireApplication.java
+++ src/main/java/com/aimit/campushire/CampushireApplication.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +21,6 @@ public class CampushireApplication {
     String home() {
         return "Hello World!";
     }
-    
     public static void main(String[] args) {
         logger.info("Running spring boot campus hire");
         SpringApplication.run(CampushireApplication.class, args);
